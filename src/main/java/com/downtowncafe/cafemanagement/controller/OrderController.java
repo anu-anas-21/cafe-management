a2d853ca4fb4967c29a1fcaa1064a4d29a511353
anu-anas-21 <anasanu0174@gmail.com>
2026-01-23 01:58:05 +0400

    Second Commit

--- src/main/java/com/downtowncafe/cafemanagement/controller/OrderController.java
+++ src/main/java/com/downtowncafe/cafemanagement/controller/OrderController.java
@@ -1,5 +1,38 @@
 package com.downtowncafe.cafemanagement.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.downtowncafe.cafemanagement.model.OrderEntity;
+import com.downtowncafe.cafemanagement.repository.OrderRepository;
+
+@RestController
+@RequestMapping("api/orders")
+@CrossOrigin(origins = "*")
 public class OrderController {
 
+    private final OrderRepository repo;
+
+    public OrderController(OrderRepository repo) {
+        this.repo = repo;
+    }
+
+    @PostMapping 
+    public OrderEntity placeOrder(@RequestBody OrderEntity order) {
+        order.setStatus("PENDING");
+        order.setOrderTime(LocalDateTime.now());
+        return repo.save(order);
+    }
+
+    @GetMapping
+    public List<OrderEntity> getOrders() {
+        return repo.findAll();
+    }
 }
